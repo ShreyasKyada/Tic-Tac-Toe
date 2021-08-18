4c8eaf2ab75df3b0a06db6503d0fd26263fa113e
ShreyasKyada <83200751+ShreyasKyada@users.noreply.github.com>
2021-08-18 21:05:20 +0530

    "SK"

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let option_btn = document.getElementById('option');
+let main = document.getElementsByClassName('option-toggle-container')[0];
+
+option_btn.addEventListener('click', () => {
+    
+});
